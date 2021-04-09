978c466869a54cfe56cc2708ef237a5bdd4b32d4
antimhr <antimhr@gmail.com>
2021-04-09 12:12:15 +0530

    first project

---
+++ banking_system/php/navbar.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <style>
+    a:link {
+      color: white;
+    }
+
+    a:hover {
+      color: white;
+      text-decoration: none;
+    }
+  </style>
+</head>
+
+<body>
+  <!--Responsive Navbar-->
+  <nav>
+    <div class="container-fluid nav-wrapper #00838f crimson darken-3 z-depth-5">
+      <a href="../index.php" class="brand-logo"><img src="https://www.flaticon.com/svg/vstatic/svg/2830/2830289.svg?token=exp=1617713396~hmac=6bc0b844ad94f982ec63928f30b03748" style="height:65px;width:55px" /></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="transfermoney.php">View Customers</a></li>
+        <li><a href="transactionhistory.php">Transactions</a></li>
+      </ul>
+    </div>
+  </nav>
+  <!--end of navbar-->
+</body>
+
+</html>
