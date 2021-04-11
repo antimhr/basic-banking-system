c9ff4466f9223992e2137036af2c922a9a7bb75f
antimhr <46750227+antimhr@users.noreply.github.com>
2021-04-11 10:55:50 +0530

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,8 @@
 
+
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "banking_system");
+$conn = mysqli_connect("localhost", "id16565095_banking_system", "LD=v!tG2EV]UH5d$", "id16565095_phpmysqldb",);
 
 if (!$conn) {
   die("Could not connect to the database due to the following error --> " . mysqli_connect_error());
